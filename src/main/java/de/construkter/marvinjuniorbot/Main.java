fad07ed2851590c647059577148dd085506f51ac
Conrad Richter <conrad.richter1@tu-dresden.de>
2026-03-17 09:21:12 +0100

    Bump version to 1.2.0

--- src/main/java/de/construkter/marvinjuniorbot/Main.java
+++ src/main/java/de/construkter/marvinjuniorbot/Main.java
@@ -31,7 +31,7 @@ public class Main extends ListenerAdapter {
 
     public static final Config CONFIG = new Config("config.properties");
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-    public static final String VERSION = "1.1.8";
+    public static final String VERSION = "1.2.0";
     public static boolean todayWasGame = false;
     public static int currentSpieltag = 0;
     public static JDA jda;

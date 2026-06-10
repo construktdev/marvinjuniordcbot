771e37169cae8b07bc95ef0d2496e42ee966fa6f
Conrad Richter <conrad.richter1@tu-dresden.de>
2026-06-10 08:31:31 +0200

    Add system logging functionality with SystemListener and SystemLogManager

---
+++ src/main/java/de/construkter/marvinjuniorbot/modules/systemLogging/SystemLogManager.java
@@ -0,0 +1,41 @@
+package de.construkter.marvinjuniorbot.modules.systemLogging;
+
+import de.construkter.marvinjuniorbot.Main;
+import de.construkter.marvinjuniorbot.logging.LoggingFileHandler;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+
+import java.util.HashMap;
+
+public class SystemLogManager {
+
+    private final TextChannel systemLogChannel = Main.CONFIG.get("systemLog") != null
+            ? Main.jda.getTextChannelById(Main.CONFIG.get("systemLog")) : null;
+
+
+    public void log(String title, String message, HashMap<String, String> args) {
+        EmbedBuilder embedBuilder = new EmbedBuilder()
+                .setTitle(title)
+                .setDescription(message + "\n\n**Additional Information:**");
+
+        for (String key : args.keySet()) {
+            String value = args.get(key);
+            if (value != null) {
+                embedBuilder.addField(key, value, false);
+            }
+        }
+
+        if (systemLogChannel != null) {
+            systemLogChannel.sendMessageEmbeds(embedBuilder.build()).queue();
+        } else {
+            StringBuilder argsString = new StringBuilder();
+
+            for (String key : args.keySet()) {
+                argsString.append(key).append(": ").append(args.get(key)).append(" // ");
+            }
+
+            LoggingFileHandler.log("systemLogChannel is null",
+                    "LogMessage: " + title + " | " + message + " | args: | " + argsString);
+        }
+    }
+}
